3aa1b4bca14d3437c56a0f0f8c4aa10c92df2597
aneethajude <aneetha.joseph@gmail.com>
2023-01-17 21:18:14 +0800

    Client Server Cookie

--- App.java
+++ App.java
@@ -1,5 +1,17 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  */
@@ -10,8 +22,39 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
+     * @throws UnknownHostException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        //do{    
+        Socket socket = new Socket("localhost",7000);
+            OutputStream os = socket.getOutputStream();
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+
+            InputStream is = socket.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+
+            Console con = System.console();
+            String readInput = "";
+            String receivedMessaged ="";
+
+            do{
+                readInput = con.readLine("Enter a command to execute ");
+                dos.writeUTF(readInput);
+                dos.flush();
+                
+                
+            }while(!readInput.equalsIgnoreCase("Close"));
+         
+      
+      if(readInput.equalsIgnoreCase("Close")){
+        
+       socket.close();}
+            // dis.close();
+            // bis.close();
+            // is.close();
     }
 }
+
